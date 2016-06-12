c755f19521a40dda83de9be7f8486c33a8d460b0
Said Sef <saidsef@gmail.com>
2016-06-12 17:35:13 +0000

    cookbook info updated

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,17 @@
 name 'es-stack'
+
 maintainer 'Said Sef'
 maintainer_email 'saidsef@gmail.com.com'
+
 license 'GPLv2'
+
 description 'Installs/Configures es-stack'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+
 version '0.1.2'
+ohai_version '~> 8'
+
+source_url 'https://github.com/saidsef/es-stack'
 
 %w{elasticsearch java apt yum nginx python}.each do |cookbook|
   depends cookbook
